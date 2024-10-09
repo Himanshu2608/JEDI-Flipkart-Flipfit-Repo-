6c1bcd0786208e63919774388568a4ef90e3ca09
XoxoPng <princhi.p@flipkart.com>
2024-10-09 11:12:24 +0530

    init code with flipfit stucture

---
+++ JEDI-Flipit-POS-Java/src/com/flipfit/client/GymFlipFitCustomerMenu.java
@@ -0,0 +1,12 @@
+
+package com.flipfit.client;
+
+import com.flipfit.business.CustomerInterface;
+import com.flipfit.business.CustomerService;
+
+public class GymFlipFitCustomerMenu {
+    CustomerInterface customerService = new CustomerService();
+
+    public void customerMainPage () {
+    }
+}
