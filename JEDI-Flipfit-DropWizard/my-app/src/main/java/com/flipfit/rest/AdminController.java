6c9841770879d59956f299040e0ff99114744b69
Praneeth Reddy <voodempraneeth@gmail.com>
2024-10-16 15:32:50 +0530

    updated admin controller

--- JEDI-Flipfit-DropWizard/my-app/src/main/java/com/flipfit/rest/AdminController.java
+++ JEDI-Flipfit-DropWizard/my-app/src/main/java/com/flipfit/rest/AdminController.java
@@ -6,12 +6,7 @@ import java.util.HashMap;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 import com.flipfit.business.FlipFitAdminBusiness;
@@ -68,6 +63,13 @@ public class AdminController {
                     return ownerMap;
                 })
                 .collect(Collectors.toList());
+    }
+
+    @POST
+    @Path("/approveOwner/ownerId={ownerId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public boolean approveOwner(@PathParam("ownerId") int ownerId) {
+        return flipFitAdminBusiness.validateOwner(ownerId);
     }
 
     @GET
